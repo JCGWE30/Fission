2c725560dbeab615080ef8abb5196ea76333f0fb
JCGWE30 <gwejc69@gmail.com>
2025-08-01 23:46:00 -0500

    Fixed presetColor

--- src/main/java/org/lepigslayer/fission/TextColor/PresetColor.java
+++ src/main/java/org/lepigslayer/fission/TextColor/PresetColor.java
@@ -3,31 +3,22 @@ package org.lepigslayer.fission.TextColor;
 import org.bukkit.ChatColor;
 import org.lepigslayer.fission.Utilities.StringUtils;
 
-public enum PresetColor {
-    RAINBOW(StringUtils::rainbowString),
-    BLACK(new BasicTextColor(ChatColor.BLACK)),
-    DARK_GREEN(new BasicTextColor(ChatColor.GREEN)),
-    DARK_RED(new BasicTextColor(ChatColor.RED)),
-    GOLD(new BasicTextColor(ChatColor.GOLD)),
-    DARK_GRAY(new BasicTextColor(ChatColor.DARK_GRAY)),
-    GREEN(new BasicTextColor(ChatColor.GREEN)),
-    RED(new BasicTextColor(ChatColor.RED)),
-    YELLOW(new BasicTextColor(ChatColor.YELLOW)),
-    DARK_BLUE(new BasicTextColor(ChatColor.BLUE)),
-    DARK_AQUA(new BasicTextColor(ChatColor.AQUA)),
-    DARK_PURPLE(new BasicTextColor(ChatColor.DARK_PURPLE)),
-    GRAY(new BasicTextColor(ChatColor.GRAY)),
-    BLUE(new BasicTextColor(ChatColor.BLUE)),
-    AQUA(new BasicTextColor(ChatColor.AQUA)),
-    LIGHT_PURPLE(new BasicTextColor(ChatColor.LIGHT_PURPLE)),
-    WHITE(new BasicTextColor(ChatColor.WHITE));
-
-    private TextColor textColor;
-    PresetColor(TextColor color){
-        this.textColor = color;
-    }
-
-    public String color(String text) {
-        return textColor.colorString(text);
-    }
+public class PresetColor {
+    public static TextColor RAINBOW = StringUtils::rainbowString;
+    public static TextColor BLACK = new BasicTextColor(ChatColor.BLACK);
+    public static TextColor DARK_GREEN = new BasicTextColor(ChatColor.DARK_GREEN);
+    public static TextColor DARK_RED = new BasicTextColor(ChatColor.DARK_RED);
+    public static TextColor GOLD = new BasicTextColor(ChatColor.GOLD);
+    public static TextColor DARK_GRAY = new BasicTextColor(ChatColor.DARK_GRAY);
+    public static TextColor GREEN = new BasicTextColor(ChatColor.GREEN);
+    public static TextColor RED = new BasicTextColor(ChatColor.RED);
+    public static TextColor YELLOW = new BasicTextColor(ChatColor.YELLOW);
+    public static TextColor DARK_BLUE = new BasicTextColor(ChatColor.DARK_BLUE);
+    public static TextColor DARK_AQUA = new BasicTextColor(ChatColor.DARK_AQUA);
+    public static TextColor DARK_PURPLE = new BasicTextColor(ChatColor.DARK_PURPLE);
+    public static TextColor GRAY = new BasicTextColor(ChatColor.GRAY);
+    public static TextColor BLUE = new BasicTextColor(ChatColor.BLUE);
+    public static TextColor AQUA = new BasicTextColor(ChatColor.AQUA);
+    public static TextColor LIGHT_PURPLE = new BasicTextColor(ChatColor.LIGHT_PURPLE);
+    public static TextColor WHITE = new BasicTextColor(ChatColor.WHITE);
 }
